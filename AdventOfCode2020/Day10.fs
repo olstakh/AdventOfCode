1beba6c719dce4f329e34f18bab6282be4decf17
Alex Stakh <olstakh@microsoft.com>
2020-12-20 15:42:03 -0800

    Day 10

---
+++ AdventOfCode2020/Day10.fs
@@ -0,0 +1,46 @@
+﻿module Day10
+
+let private getAllJoltsSorted (fileContent : string[]) =
+    let jolts = fileContent |> Array.map int
+    let maxJolt = jolts |> Array.max
+    let startJolt = 0
+    let finalJolt = maxJolt + 3
+
+    jolts
+    |> Array.append [| startJolt; finalJolt |]
+    |> Array.sort
+
+let Solve1 (fileContent : string[]) =
+    let joltDiff =
+        fileContent
+        |> getAllJoltsSorted
+        |> Seq.pairwise
+        |> Seq.map(fun (a, b) -> b - a)
+
+    let diff1 = joltDiff |> Seq.filter((=) 1) |> Seq.length
+    let diff3 = joltDiff |> Seq.filter((=) 3) |> Seq.length
+
+    diff1 * diff3
+
+let Solve2 (fileContent : string[]) =
+    let allJolts = fileContent |> getAllJoltsSorted
+    let lastJolt = allJolts |> Seq.last
+
+    let WaysToConnectJolts prevWaysToConnectJolts currentJolt =
+        let newConnection =
+            prevWaysToConnectJolts
+            |> List.sumBy(fun (prevJolt, ways) ->
+                if (1 <= prevJolt - currentJolt && prevJolt - currentJolt <= 3)
+                    then ways else 0L
+            )
+
+        [(currentJolt, newConnection)]
+        |> List.append prevWaysToConnectJolts
+        |> List.skip 1
+
+    allJolts
+    |> Seq.rev
+    |> Seq.skip 1
+    |> Seq.fold WaysToConnectJolts [(lastJolt, 0L); (lastJolt, 0L); (lastJolt, 1L)]
+    |> List.last
+    |> fun (firstJolt, waysToConnect) -> waysToConnect
--- AdventOfCode2020/Program.fs
+++ AdventOfCode2020/Program.fs
@@ -8,7 +8,7 @@ let main argv =
     |> Array.tryExactlyOne
     |> Option.defaultValue("..\..\..\Input.txt")
     |> File.ReadAllLines
-    |> Day9.Solve2
+    |> Day10.Solve2
     |> printfn "%d"
 
     0 // return an integer exit code
