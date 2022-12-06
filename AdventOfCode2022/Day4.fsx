664ea17cbbe4997b9479340807ce99e9b9416167
Alex Stakh <olstakh@microsoft.com>
2022-12-06 11:09:55 -0800

    Day4

---
+++ AdventOfCode2022/Day4.fsx
@@ -0,0 +1,65 @@
+#load "Scripts\\ParserLibrary.fsx"
+open ParserLibrary
+
+module Day4 =
+
+    type Segment =
+        {
+            Start : int
+            Finish : int
+        } with
+            static member Construct (start, finish) =
+                {
+                    Start = start
+                    Finish = finish
+                }
+            // Ex: 10-20
+            static member Parser =
+                pint .>> pchar '-' .>>. pint |>> Segment.Construct
+                <?> "Segment"
+
+    type SegmentPair =
+        {
+            Segment1 : Segment
+            Segment2 : Segment
+        } with
+            static member Construct (segment1, segment2) =
+                {
+                    Segment1 = segment1
+                    Segment2 = segment2
+                }
+            // Ex: 10-20,25-35
+            static member Parser =
+                Segment.Parser .>> pchar ',' .>>. Segment.Parser |>> SegmentPair.Construct
+                <?> "Segment pair"
+
+    let Parse (s:string) =
+        match (run SegmentPair.Parser s) with
+            | Success (segmentPair, remainingInput) -> segmentPair
+            | Failure (label, error, pos) -> failwithf "Failed to parse input. Label: %s, Error : %s, position : %d" label error pos.column
+
+    let InputData =
+        "Input.txt"
+        |> System.IO.File.ReadAllLines
+        |> Array.map Parse
+ 
+    let Solve filterCondition =
+        InputData
+        |> Array.filter filterCondition
+        |> Array.length
+
+    module Task1 =
+        let oneSegmentFullyContainsAnother segmentPair =
+            segmentPair.Segment1.Start <= segmentPair.Segment2.Start && segmentPair.Segment2.Finish <= segmentPair.Segment1.Finish
+            ||
+            segmentPair.Segment2.Start <= segmentPair.Segment1.Start && segmentPair.Segment1.Finish <= segmentPair.Segment2.Finish
+
+        let Answer = Solve oneSegmentFullyContainsAnother
+
+    module Task2 =
+        let nonOverlapCheck segmentPair =
+            segmentPair.Segment1.Start > segmentPair.Segment2.Finish || segmentPair.Segment1.Finish < segmentPair.Segment2.Start
+            ||
+            segmentPair.Segment2.Start > segmentPair.Segment1.Finish || segmentPair.Segment2.Finish < segmentPair.Segment1.Start
+        
+        let Answer = Solve (nonOverlapCheck >> not)
